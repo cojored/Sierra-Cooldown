0df05937cdb04800cf05c7d973d050d9f2da6171
cojored <66964845+cojored@users.noreply.github.com>
2020-09-18 09:21:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ function sleep(ms) {
     resolve => setTimeout(resolve, ms)
   );
 }
-async function cooldown(id, cooldown) {
-  if (isNaN(id)) throw new TypeError("Please enter a number");
-  if (isNaN(cooldown)) throw new TypeError("Please enter a number");
+async function cooldownID(id, cooldown) {
+  if (isNaN(id)) throw new TypeError("Please enter a number for the ID");
+  if (isNaN(cooldown)) throw new TypeError("Please enter a number for the cooldown");
   await sleep(cooldown)
 	 return `<@${id}> your cooldown is over` 
 };
 
+async function cooldown(message, cooldown) {
+  if (typeof message != 'string') throw new TypeError("Please enter a string for the message");
+  if (isNaN(cooldown)) throw new TypeError("Please enter a number for the cooldown");
+  await sleep(cooldown)
+	 return `${message}` 
+};
 
-module.exports = {cooldown}
+
+module.exports = {cooldown, cooldownID}
